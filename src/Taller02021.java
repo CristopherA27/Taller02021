7ea9f81893c486a0c867b4b7dd7bb6463819521a
Cristobal Ríos <cristobalrios2001@gmail.com>
2021-09-13 16:59:08 -0300

    no hay chance

--- src/Taller02021.java
+++ src/Taller02021.java
@@ -4,6 +4,8 @@ public class Taller02021 {
 	public static void main(String[] args) {
 		System.out.println("jasfhsdfhdsf");
 		System.out.println("que pasa loco");
+                System.out.println("dale loco te carreo en el valo y aqui");
+                System.out.println("no hay chance");
 
 	}
 
