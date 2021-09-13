a5008688f52d3d5060dd607c997d0ca24f1898e8
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2021-09-13 20:22:10 -0300

    lecturas
    hice algunas lecturas#avanzando

--- src/Taller02021.java
+++ src/Taller02021.java
@@ -1,9 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Taller02021 {
+	
+	public static int leerClientes(String [] lnombres,String [] lapellidos,String [] lruts,String [] lcontraseñas,int [] lsaldos) throws FileNotFoundException {
+		Scanner s = new Scanner(new File("clientes.txt"));
+		int cont = 0;
+		while(s.hasNextLine()) {
+			String line = s.nextLine();
+			String [] partes = line.split(",");
+			String nombre = partes[0];
+			String apellido = partes[1];
+			String rut = partes[2];
+			String contraseña = partes[3];
+			int saldo = Integer.parseInt(partes[4]);
+			lnombres[cont] = nombre;
+			lapellidos[cont] = apellido;
+			lruts[cont] = rut;
+			lcontraseñas[cont] = contraseña;
+			lsaldos[cont] = saldo;
+			cont++;
+		}
+		s.close();
+		return cont;
+	}
+	
+	public static void leerstatus(String [] listaPaseMovilidad) throws FileNotFoundException {
+		Scanner s = new Scanner(new File("status.txt"));
+		int cont = 0;
+		while(s.hasNextLine()) {
+			String line = s.nextLine();
+			String [] partes = line.split(",");
+			String paseMovilidad = partes[0];
+			String rut = partes[1];
+			listaPaseMovilidad[cont] = paseMovilidad;
+			cont++;
+		}
+		s.close();		
+	}
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		String [] lnombres = new String[200];
+		String [] lapellidos  = new String[200];
+		String [] lruts = new String[200];
+		String [] lcontraseñas = new String[200];
+		int [] lsaldos = new int[200];
+		int cantClientes = leerClientes(lnombres, lapellidos, lruts, lcontraseñas, lsaldos);
+		String [] listaPaseMovilidad = new String[cantClientes];
+
 		System.out.println("Ahora si estamos listos para empezar");
-
 	}
 
 }
