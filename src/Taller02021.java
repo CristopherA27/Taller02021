280ce332643cbafb086bcdab8658190d1297b65b
CristopherA27 <76230505+CristopherA27@users.noreply.github.com>
2021-09-13 17:09:20 -0300

    cambioFinalTodolisto

--- src/Taller02021.java
+++ src/Taller02021.java
@@ -2,10 +2,7 @@
 public class Taller02021 {
 
 	public static void main(String[] args) {
-		System.out.println("jasfhsdfhdsf");
-		System.out.println("que pasa loco");
-                System.out.println("dale loco te carreo en el valo y aqui");
-                System.out.println("no hay chance");
+		System.out.println("Ahora si estamos listos para empezar");
 
 	}
 
